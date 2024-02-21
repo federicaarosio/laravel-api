400cbe6ffec62e0dbb6a3eefe280ca5d375ae747
federicaarosio <arosio.fede@gmail.com>
2024-02-21 19:16:10 +0100

    modified ProjectController for show method

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -17,6 +17,7 @@ class ProjectController extends Controller
     }
 
     public function show(Project $project){
+        $project = Project::with('type', 'technologies', 'socials')->findOrFail($project->id);
         return response()->json([
             "success" => true,
             "results" => $project
